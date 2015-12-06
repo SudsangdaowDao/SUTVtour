14256c0fd24bd64cabdafefb178a45c4134f8d2f
dao <sudsangdaow.duangthai@gmail.com>
2015-12-06 15:41:41 +0700

    commit first time

--- app/src/main/java/com/ss/example/sutvtour/MainActivity.java
+++ app/src/main/java/com/ss/example/sutvtour/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ss.example.sutvtour;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView) findViewById(R.id.image);
+        image.setBackgroundResource(R.drawable.rotation_android);
+
+        AnimationDrawable anim = (AnimationDrawable) image.getBackground();
+        anim.start();
     }
 }
